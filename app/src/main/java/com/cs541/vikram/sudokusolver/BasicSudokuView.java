d0315c684254ea98d1e5263fb3a2f28800225052
Vikram Ramesh <vikram@vikrams-mbp-2.ftrdhcpuser.net>
2015-03-02 23:11:02 -0800

    User can now select cells and they get highlighted

--- app/src/main/java/com/cs541/vikram/sudokusolver/BasicSudokuView.java
+++ app/src/main/java/com/cs541/vikram/sudokusolver/BasicSudokuView.java
@@ -1,9 +1,12 @@
 package com.cs541.vikram.sudokusolver;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -11,11 +14,17 @@ import android.view.View;
  */
 public class BasicSudokuView extends View {
 
-    private static final String TAG = "SudokuSolver";
-    private static int DIM = 9;
-    private static int BLOCK_SIZE = 3;
+    private final String TAG = "SudokuSolver";
+    private final int DIM = 9;
+    private final int BLOCK_SIZE = 3;
+
+    private int WIDTH;
+    private int HEIGHT;
+    private Rect clickedCell;
+
     Paint lineColor = new Paint();
     Paint gridColor = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint clickedColor = new Paint();
 
 
 
@@ -33,14 +42,37 @@ public class BasicSudokuView extends View {
         lineColor.setStyle(Paint.Style.STROKE);
         lineColor.setColor(getResources().getColor(R.color.Black));
 
+        clickedCell = new Rect();
+        clickedColor.setColor(getResources().getColor(R.color.Red));
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            float x = event.getX();
+            float y = event.getY();
+            Log.v(TAG, "Touched (" + x + ", " + y + ")");
+            //figure out which square it belongs to, and create a rectangle that we can color later
+
+            Cell selected = new Cell(x, y);
+            Log.v(TAG, "GOT Cell" + "[" + selected.row + "," + selected.column + "]");
+            clickedCell = selected.getRect();
+            invalidate(clickedCell); //refreshes just this cell. invalidate() takes too long
+
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+
 
     @Override
     protected void onDraw(Canvas canvas){
         Log.v(TAG, "Drawing Board");
         //set the size of the sudoku grid
-        final int WIDTH = getWidth();
-        final int HEIGHT = new Long(Math.round(getHeight() * .90)).intValue();
+        WIDTH = getWidth();
+        HEIGHT = new Long(Math.round(getHeight() * .90)).intValue();
         final float CELL_HEIGHT = HEIGHT / DIM;
         final float CELL_WIDTH = WIDTH / DIM;
 
@@ -60,16 +92,41 @@ public class BasicSudokuView extends View {
             }
             //horiz line
             canvas.drawLine(0, i * CELL_HEIGHT, WIDTH, i * CELL_HEIGHT, lineColor); //startx, starty, endx, endy
-            Log.v(TAG, "Drawing " + lineColor.getStrokeWidth() + "line from (" + 0 + "," + i * CELL_HEIGHT + ") to (" + WIDTH + "," + i *CELL_HEIGHT + ")");
             //vert line
             canvas.drawLine(i * CELL_WIDTH, 0, i * CELL_WIDTH, HEIGHT, lineColor);
-            Log.v(TAG, "Drawing " + lineColor.getStrokeWidth() + "line from (" + i * CELL_WIDTH + "," + 0 + ") to (" + i * CELL_WIDTH + "," + HEIGHT + ")");
 
         }
 
+        //draw clicked cell if possible
+        Log.v(TAG, "here");
+        canvas.drawRect(clickedCell, clickedColor);
+
     }
 
+    private class Cell {
+        int row;
+        int column;
 
+        public Cell(float x, float y){
+            //height/dim and width/dim are the cell height and cell width respectively
+            this.row = Double.valueOf(Math.floor( y / (HEIGHT / DIM) )).intValue();
+            this.column = Double.valueOf(Math.floor( x / (WIDTH / DIM) )).intValue();
+        }
+
+        public Rect getRect(){
+            Rect rect = new Rect();
+            int CELL_WIDTH = WIDTH / DIM;
+            int CELL_HEIGHT = HEIGHT / DIM;
+            //left, top, right, bottom
+            int left = column * CELL_WIDTH;
+            int top = row * CELL_HEIGHT;
+            rect.set(left, top, left + CELL_WIDTH, top + CELL_HEIGHT);
+            Log.v(TAG,"Rect: " + rect.toShortString());
+            return rect;
+        }
+
+
+    }
 
 
 }
